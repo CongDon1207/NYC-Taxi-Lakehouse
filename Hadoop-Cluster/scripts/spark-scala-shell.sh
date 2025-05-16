d65db989a5201f0ca12e6756678707b56a5da38d
huy-dataguy <quochuy.working@gmail.com>
2025-05-16 16:21:28 +0700

    #3 update Delta Lake package to Scala 2.13

--- Hadoop-Cluster/scripts/spark-scala-shell.sh
+++ Hadoop-Cluster/scripts/spark-scala-shell.sh
@@ -1,1 +1,1 @@
-spark-shell --packages io.delta:delta-core_2.12:2.4.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+spark-shell --packages io.delta:delta-core_2.13:2.4.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
--- Hadoop-Cluster/scripts/spark-sql.sh
+++ Hadoop-Cluster/scripts/spark-sql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spark-sql --packages io.delta:delta-core_2.12:2.4.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
+spark-sql --packages io.delta:delta-core_2.13:2.4.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog"
